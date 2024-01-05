a1f0778e3320f552f25fa452208f258a427bc432
MUKESH KUMAR SAH <121852608+Mukeshkumar529@users.noreply.github.com>
2024-01-05 14:01:56 +0530

    Update server.js

--- server.js
+++ server.js
@@ -32,6 +32,13 @@ app.use(morgan("dev"));
 app.use("/api/v1/auth", require("./routes/userRoutes"));
 app.use("/api/v1/post", require("./routes/postRoutes"));
 
+// home
+app.get("/", (req,res) => {
+  res.status(200).send({
+    "success":true,
+    "message":"Node Server Running"
+  })
+
 // port
 const PORT = process.env.PORT || 8080;
 
